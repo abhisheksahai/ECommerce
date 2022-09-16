c5c502d57899cbc75f873b1b029b59499de8fa3a
Abhishek Sahai <abhishek.sahai@outlook.in>
2022-09-16 15:31:23 +0530

    Update Catalog.tsx

--- Client/src/features/Catalog.tsx
+++ Client/src/features/Catalog.tsx
@@ -1,15 +1,18 @@
 import { useState, useEffect } from "react";
 import { iproduct } from "../app/models/iproduct";
 import { ProductList } from "./catalog/ProductList";
+import axios from "axios";
 
 export function Catalog() {
   const [products, setProducts] = useState<iproduct[]>([]);
 
   useEffect(() => {
-    fetch("https://localhost:7075/api/Products/GetProducts")
-      .then((respose) => respose.json())
-      .then((data) => setProducts(data));
+    axios
+      .get(`https://localhost:7075/api/Products/GetProducts`)
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error));
   }, []);
+
   return (
     <>
       <ProductList products={products} />
