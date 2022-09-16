01827e12f7764a00e50b62e75de0beb1a71d71a7
Abhishek Sahai <abhishek.sahai@outlook.in>
2022-09-16 15:26:56 +0530

    Added axios for api call

--- Client/src/app/layout/Header.tsx
+++ Client/src/app/layout/Header.tsx
@@ -2,6 +2,7 @@ import { ShoppingCart } from "@mui/icons-material";
 import {
   AppBar,
   Badge,
+  Box,
   IconButton,
   List,
   ListItem,
@@ -27,53 +28,89 @@ const rightLinks = [
   { title: "register", path: "/register" },
 ];
 
+const navStyles = {
+  color: "inherit",
+  typography: "h6",
+  textDecoration: "none",
+  "&:hover": {
+    color: "grey.500",
+  },
+  "&.active": {
+    color: "text.secondary",
+  },
+};
+
 export function Header({ darkMode, onThemeChange }: Props) {
   return (
     <>
       <AppBar position="static" sx={{ mb: 4 }}>
-        <Toolbar>
-          <Typography
-            variant="h6"
-            component={NavLink}
-            to="/"
-            sx={{ color: "inherit", textDecoration: "none" }}
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
           >
-            ECommerce
-          </Typography>
-          <Switch
-            checked={darkMode}
-            color="secondary"
-            onChange={onThemeChange}
-          />
-          <List sx={{ display: "flex" }}>
-            {midLinks.map(({ title, path }) => (
-              <ListItem
-                component={NavLink}
-                to={path}
-                key={path}
-                sx={{ color: "inherit", typography: "h6" }}
-              >
-                {title.toUpperCase()}
-              </ListItem>
-            ))}
-          </List>
-          <IconButton sx={{ color: "inherit" }}>
-            <Badge badgeContent={4} color="secondary">
-              <ShoppingCart />
-            </Badge>
-          </IconButton>
-          <List sx={{ display: "flex" }}>
-            {rightLinks.map(({ title, path }) => (
-              <ListItem
-                component={NavLink}
-                to={path}
-                key={path}
-                sx={{ color: "inherit", typography: "h6" }}
-              >
-                {title.toUpperCase()}
-              </ListItem>
-            ))}
-          </List>
+            <Typography variant="h6" component={NavLink} to="/" sx={navStyles}>
+              ECommerce
+            </Typography>
+            <Switch
+              checked={darkMode}
+              color="secondary"
+              onChange={onThemeChange}
+            />
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <List sx={{ display: "flex" }}>
+              {midLinks.map(({ title, path }) => (
+                <ListItem
+                  component={NavLink}
+                  to={path}
+                  key={path}
+                  sx={navStyles}
+                >
+                  {title.toUpperCase()}
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <IconButton sx={{ color: "inherit" }}>
+              <Badge badgeContent={4} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+            <List sx={{ display: "flex" }}>
+              {rightLinks.map(({ title, path }) => (
+                <ListItem
+                  component={NavLink}
+                  to={path}
+                  key={path}
+                  sx={navStyles}
+                >
+                  {title.toUpperCase()}
+                </ListItem>
+              ))}
+            </List>
+          </Box>
         </Toolbar>
       </AppBar>
     </>
--- Client/src/features/catalog/ProductDetails.tsx
+++ Client/src/features/catalog/ProductDetails.tsx
@@ -1,5 +1,24 @@
 import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { iproduct } from "../../app/models/iproduct";
 
 export function ProductDetails() {
-  return <Typography variant="h2">Product details</Typography>;
+  const { id } = useParams<{ id: string }>();
+  const [product, setProduct] = useState<iproduct | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`https://localhost:7075/api/Products/GetProduct/${id}`)
+      .then((response) => setProduct(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  if (loading) return <h3>Loading...</h3>;
+  if (!product) return <h3>Product not found</h3>;
+
+  return <Typography variant="h2">Product details : {product.name}</Typography>;
 }
