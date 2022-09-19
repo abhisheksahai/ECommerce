0d0f21fc058e4402439f81325174055f37b55789
Abhishek Sahai <abhishek.sahai@outlook.in>
2022-09-19 12:35:00 +0530

    Basic implementation of redux

--- Client/src/features/counter/Counter.tsx
+++ Client/src/features/counter/Counter.tsx
@@ -1,3 +1,34 @@
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  CounterState,
+  DECREMENT_COUNTER,
+  INCREMENT_COUNTER,
+} from "./counterReducer";
+
 export default function Counter() {
-  return <></>;
+  const dispatch = useDispatch();
+  const { data, title } = useSelector((state: CounterState) => state);
+  return (
+    <>
+      <Typography variant="h2">{title}</Typography>
+      <Typography variant="h5">The data is {data}</Typography>
+      <ButtonGroup>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={() => dispatch({ type: DECREMENT_COUNTER })}
+        >
+          Decrement
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => dispatch({ type: INCREMENT_COUNTER })}
+        >
+          Increment
+        </Button>
+      </ButtonGroup>
+    </>
+  );
 }
